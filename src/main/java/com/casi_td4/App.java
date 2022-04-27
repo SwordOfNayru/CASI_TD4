adeae954d5088d711883aa756e74ec6dec117280
SwordOfNayru <theswordoflink@live.com>
2022-04-27 09:32:53 +0200

    init commit

---
+++ src/main/java/com/casi_td4/App.java
@@ -0,0 +1,13 @@
+package com.casi_td4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
